bac9eae60837c723e28327ecf55931f3ebda86f1
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2024-09-23 16:23:38 -0500

    test: test for player movement

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -11,7 +11,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	handle_player_input(delta)
+	move_and_slide()
 func handle_player_input(delta):
-	var direction : Vector2 = Input.get_vector("move_left","move_right","move_down","move_up")
+	var direction : Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
 	self.velocity.x = lerp(velocity.x, speed * direction.x, acceleration * delta)
 	self.velocity.y = lerp(velocity.y, speed * direction.y, acceleration * delta)
