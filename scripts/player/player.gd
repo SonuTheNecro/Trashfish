fd43409b519fd9007e7235e9939361defc8032a1
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2024-09-23 17:52:34 -0500

    feat: player w/ basic sprite

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -11,8 +11,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	handle_player_input(delta)
+	handle_player_animation()
 	move_and_slide()
 func handle_player_input(delta):
 	var direction : Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
 	self.velocity.x = lerp(velocity.x, speed * direction.x, acceleration * delta)
 	self.velocity.y = lerp(velocity.y, speed * direction.y, acceleration * delta)
+
+func handle_player_animation():
+	if abs(velocity.x) > 0.001:
+		flip(velocity.x < 0)
+# Flip the animations and hitboxes
+func flip(value: bool):
+	if value != $AnimatedSprite2D.flip_h:
+		$AnimatedSprite2D.flip_h = value
