50fe87eb375d7fbc40e1cfd76e6728480e2316eb
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2024-10-13 16:30:30 -0500

    refactor: added a variable to determine if attacking

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,6 +3,10 @@ const speed : int = 200
 const acceleration : int = 20
 @export var health : int = 6
 
+
+var isAttacking : bool = false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,7 +14,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attack") and not isAttacking:
 		self.attack()
 	
 	if self.health == 0:
@@ -31,6 +35,7 @@ func handle_player_animation():
 func flip(value: bool):
 	if value != $AnimatedSprite2D.flip_h:
 		$AnimatedSprite2D.flip_h = value
+		$attack_hitbox/CollisionShape2D.global_position.x *= -1
 
 func set_health(change : int):
 	self.health = change
@@ -46,11 +51,15 @@ func player_death():
 	print("ded")
 	
 func attack():
+	#print("just_attacked!")
 	get_node("attack_hitbox/CollisionShape2D").set_deferred("disabled", false)
+	isAttacking = true
 	#$AnimatedSprite2D.play("attack")
+	$attack_hitbox/attack_timer.start()
 	
 
 
 
 func _on_attack_timer_timeout() -> void:
 	get_node("attack_hitbox/CollisionShape2D").set_deferred("disabled", true)
+	isAttacking = false
