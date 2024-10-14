ced7c09258599a3f5bba49196702eaf22ec0905c
Sonu <156054782+SonuTheNecro@users.noreply.github.com>
2024-10-14 15:57:57 -0500

    feat: delete component

---
+++ scripts/misc/delete_component.gd
@@ -0,0 +1,18 @@
+extends Node2D
+@export var delete_time : float
+var delete_timer : Timer
+# Purpose of this component is to automatically delete things when they aren't needed anymore
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	delete_timer = Timer.new()
+	delete_timer.wait_time = delete_time
+	delete_timer .one_shot = true
+	self.add_child(delete_timer)
+	delete_timer.timeout.connect(_on_delete_timer_timeout)
+	delete_timer.start()
+
+
+func _on_delete_timer_timeout() -> void:
+	self.get_parent().queue_free()
---
+++ scripts/player/trash_can.gd
@@ -0,0 +1,22 @@
+extends Node2D
+@export var rotation_speed : int = 0
+@export var timer_start : float = 0
+var timer : Timer
+var delete_timer : Timer
+func _ready() -> void:
+	# Creating this as a child to the player node
+	self.global_position.x = -1000
+	timer = Timer.new()
+	timer.wait_time = timer_start
+	timer.one_shot = true
+	self.add_child(timer)
+	timer.timeout.connect(_on_active_timer_timeout)
+	timer.start()
+
+
+func _process(delta: float) -> void:
+	self.rotation_degrees += rotation_speed
+
+func _on_active_timer_timeout() -> void:
+	self.global_position = get_parent().global_position
+	
