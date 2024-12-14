88397c2340d761ff74bed215ddeb2fb1f9e50b74
Sonu <bhaarat.mohanta@gmail.com>
2024-12-14 15:05:27 -0600

    feat: actual main menu yippie!

--- scripts/world/start_button.gd
+++ scripts/world/start_button.gd
@@ -1,13 +1,6 @@
 extends Node2D
 
 
-
-func _ready():
-	pass # Replace with function body.
-
-func _process(delta):
-	pass
-
 func attacked():
 	get_tree().change_scene_to_file("res://scenes/world/world.tscn")
 	
